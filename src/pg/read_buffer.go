759d6c53bac465cebb8c5eaa35007689c5f52d55
Denis Diachkov <d.diachkov@gmail.com>
2019-11-11 16:25:55 -0800

    Introduce pg.ReadBuffer to decode libpq messages

---
+++ src/pg/read_buffer.go
@@ -0,0 +1,143 @@
+package pg
+
+import (
+	"bytes"
+	"encoding/binary"
+	"encoding/hex"
+	"fmt"
+	"io"
+)
+
+// ReadBuffer provides bufio.Scanner-like API to decode PG data types described here:
+// https://www.postgresql.org/docs/9.6/protocol-message-types.html
+//
+// NB: buffer is read-only! You cannot reset the state!
+type ReadBuffer []byte
+
+//
+// Main use case:
+//
+//   buf := make(PgReadBuffer, 31337) // Allocate buffer
+//   io.ReadFull(r, buf)              // Write data to the buffer
+//   value, err := buf.ReadInt32()    // Start decoding
+//   value, err := buf.ReadString()
+//   ...
+//
+
+// Len returns the number of bytes left in the buffer.
+func (b *ReadBuffer) Len() int {
+	return len(*b)
+}
+
+// ReadBytes reads and returns exactly n bytes from the buffer. The bytes are removed from the buffer afterwards.
+// If there are not enough bytes in the buffer available, it returns error io.EOF and does not advance.
+func (b *ReadBuffer) ReadBytes(n int) ([]byte, error) {
+	if b.Len() < n {
+		return nil, io.EOF
+	}
+
+	bytesRead := (*b)[:n]
+
+	// Advance the slice: n = 3, b = { 1, 2, 3, 4, 5 } -> b = { 4, 5 }
+	*b = (*b)[n:]
+
+	return bytesRead, nil
+}
+
+// ReadByte reads and returns the next byte from the buffer. The byte is removed from the buffer afterwards.
+// If no byte is available, it returns error io.EOF.
+func (b *ReadBuffer) ReadByte() (byte, error) {
+	value, err := b.ReadBytes(1)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return value[0], nil
+}
+
+// ReadInt32 decodes a 32-bit signed integer and advances the buffer over it.
+// If there are not enough bytes in the buffer available, it returns error io.EOF and does not advance.
+func (b *ReadBuffer) ReadInt32() (int32, error) {
+	intBytes, err := b.ReadBytes(4)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return int32(binary.BigEndian.Uint32(intBytes)), nil
+}
+
+// ReadInt16 decodes a 16-bit signed integer and advances the buffer over it.
+// If there are not enough bytes in the buffer available, it returns error io.EOF and does not advance.
+func (b *ReadBuffer) ReadInt16() (int16, error) {
+	intBytes, err := b.ReadBytes(2)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return int16(binary.BigEndian.Uint16(intBytes)), nil
+}
+
+// ReadString reads a null-terminated string and advances the buffer over it.
+// If there is no terminator present in the buffer, it returns error and does not advance.
+func (b *ReadBuffer) ReadString() (string, error) {
+	// Find the first occurence of \0x00
+	nullIndex := bytes.IndexByte(*b, 0)
+
+	if nullIndex < 0 {
+		return "", fmt.Errorf("invalid message format; expected string terminator")
+	}
+
+	// Read string bytes including \0x00
+	stringBytes, err := b.ReadBytes(nullIndex + 1)
+
+	if err != nil {
+		return "", err
+	}
+
+	// Convert to string removing \0x00
+	return string(stringBytes[:nullIndex]), nil
+}
+
+// ReadInt32Array decodes an array of k 32-bit signed integers and advances the buffer over it.
+// If there are not enough bytes in the buffer available, it returns error io.EOF and does not advance.
+func (b *ReadBuffer) ReadInt32Array(k int) ([]int32, error) {
+	arrBytes, err := b.ReadBytes(k * 4)
+
+	if err != nil {
+		return nil, err
+	}
+
+	arr := make([]int32, k)
+
+	for i := range arr {
+		arr[i] = int32(binary.BigEndian.Uint32(arrBytes[4*i:]))
+	}
+
+	return arr, nil
+}
+
+// ReadInt16Array decodes an array of k 16-bit signed integers and advances the buffer over it.
+// If there are not enough bytes in the buffer available, it returns error io.EOF and does not advance.
+func (b *ReadBuffer) ReadInt16Array(k int) ([]int16, error) {
+	arrBytes, err := b.ReadBytes(k * 2)
+
+	if err != nil {
+		return nil, err
+	}
+
+	arr := make([]int16, k)
+
+	for i := range arr {
+		arr[i] = int16(binary.BigEndian.Uint16(arrBytes[2*i:]))
+	}
+
+	return arr, nil
+}
+
+// Inspect returns content of the buffer as a hex dump for debug purposes.
+func (b *ReadBuffer) Inspect() string {
+	return fmt.Sprintf("buffer length: %d\n%s", b.Len(), hex.Dump(*b))
+}
---
+++ src/pg/read_buffer_test.go
@@ -0,0 +1,170 @@
+package pg
+
+import (
+	"io"
+	"testing"
+
+	"gotest.tools/assert"
+	is "gotest.tools/assert/cmp"
+)
+
+func TestLen(t *testing.T) {
+	buf := make(ReadBuffer, 42)
+	assert.Equal(t, buf.Len(), 42)
+}
+
+func TestReadBytes(t *testing.T) {
+	buf := ReadBuffer{0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xE1, 0xDE, 0xAD}
+
+	// Regular read
+	bytesRead, err := buf.ReadBytes(4)
+
+	assert.NilError(t, err)
+	assert.DeepEqual(t, bytesRead, []byte{0xDE, 0xAD, 0xBE, 0xEF})
+	assert.Equal(t, buf.Len(), 4) // buffer advances
+
+	// Not enough bytes
+	_, err = buf.ReadBytes(100)
+
+	assert.ErrorType(t, err, io.EOF)
+	assert.Equal(t, buf.Len(), 4) // buffer doesn't advance
+}
+
+func TestReadByte(t *testing.T) {
+	buf := ReadBuffer{0xFF}
+
+	// Regular read
+	byteRead, err := buf.ReadByte()
+
+	assert.NilError(t, err)
+	assert.Equal(t, byteRead, byte(0xFF))
+	assert.Equal(t, buf.Len(), 0) // buffer advances
+
+	// Buffer is empty
+	_, err = buf.ReadByte()
+
+	assert.ErrorType(t, err, io.EOF)
+}
+
+func TestReadInt32(t *testing.T) {
+	buf := ReadBuffer{
+		// 31337 in Big-endian
+		0x0, 0x0, 0x7A, 0x69,
+
+		// 2 bytes — not enough for 32 bit integer
+		0xCA, 0xFE,
+	}
+
+	// Regular read
+	num, err := buf.ReadInt32()
+
+	assert.NilError(t, err)
+	assert.Equal(t, num, int32(31337))
+	assert.Equal(t, buf.Len(), 2) // buffer advances
+
+	// Not enough bytes
+	_, err = buf.ReadInt32()
+
+	assert.ErrorType(t, err, io.EOF)
+	assert.Equal(t, buf.Len(), 2) // buffer doesn't advance
+}
+
+func TestReadInt16(t *testing.T) {
+	buf := ReadBuffer{
+		// 42 in Big-endian
+		0x0, 0x2A,
+
+		// 1 bytes — not enough for 16 bit integer
+		0xFF,
+	}
+
+	// Regular read
+	num, err := buf.ReadInt16()
+
+	assert.NilError(t, err)
+	assert.Equal(t, num, int16(42))
+	assert.Equal(t, buf.Len(), 1) // buffer advances
+
+	// Not enough bytes
+	_, err = buf.ReadInt16()
+
+	assert.ErrorType(t, err, io.EOF)
+	assert.Equal(t, buf.Len(), 1) // buffer doesn't advance
+}
+
+func TestReadString(t *testing.T) {
+	buf := ReadBuffer{
+		'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', 0x00, // 12 bytes
+		'n', 'o', ' ', 't', 'e', 'r', 'm', 'i', 'n', 'a', 't', 'o', 'r', // 13 bytes
+	}
+
+	// Regular read
+	str, err := buf.ReadString()
+
+	assert.NilError(t, err)
+	assert.Equal(t, str, "hello world")
+	assert.Equal(t, buf.Len(), 13) // buffer advances
+
+	// Cannot find null byte
+	_, err = buf.ReadString()
+
+	assert.ErrorContains(t, err, "expected string terminator")
+	assert.Equal(t, buf.Len(), 13) // buffer doesn't advance
+}
+
+func TestReadInt32Array(t *testing.T) {
+	buf := ReadBuffer{
+		// 1, 2, 3, 4, 5, 6 in Big-endian
+		0x0, 0x0, 0x0, 0x1,
+		0x0, 0x0, 0x0, 0x2,
+		0x0, 0x0, 0x0, 0x3,
+		0x0, 0x0, 0x0, 0x4,
+		0x0, 0x0, 0x0, 0x5,
+		0x0, 0x0, 0x0, 0x6,
+	}
+
+	// Regular read
+	arr, err := buf.ReadInt32Array(5)
+
+	assert.NilError(t, err)
+	assert.DeepEqual(t, arr, []int32{1, 2, 3, 4, 5})
+	assert.Equal(t, buf.Len(), 4) // buffer advances
+
+	// Not enough bytes
+	_, err = buf.ReadInt32Array(100)
+
+	assert.ErrorType(t, err, io.EOF)
+	assert.Equal(t, buf.Len(), 4) // buffer doesn't advance
+}
+
+func TestReadInt16Array(t *testing.T) {
+	buf := ReadBuffer{
+		// 6, 5, 4, 3, 2, 1 in Big-endian
+		0x0, 0x6,
+		0x0, 0x5,
+		0x0, 0x4,
+		0x0, 0x3,
+		0x0, 0x2,
+		0x0, 0x1,
+	}
+
+	// Regular read
+	arr, err := buf.ReadInt16Array(5)
+
+	assert.NilError(t, err)
+	assert.DeepEqual(t, arr, []int16{6, 5, 4, 3, 2})
+	assert.Equal(t, buf.Len(), 2) // buffer advances
+
+	// Not enough bytes
+	_, err = buf.ReadInt16Array(100)
+
+	assert.ErrorType(t, err, io.EOF)
+	assert.Equal(t, buf.Len(), 2) // buffer doesn't advance
+}
+
+func TestInspect(t *testing.T) {
+	buf := ReadBuffer{0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xE1, 0xDE, 0xAD}
+	output := buf.Inspect()
+
+	assert.Assert(t, is.Contains(output, "00000000  de ad be ef fe e1 de ad"))
+}
