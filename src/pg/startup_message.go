a58a29536495231df84ccbd7788aad95f7c78336
Denis Diachkov <d.diachkov@gmail.com>
2019-11-17 13:07:47 -0800

    Add StartupMessage struct

---
+++ src/pg/startup_message.go
@@ -0,0 +1,47 @@
+package pg
+
+// SSLMagic is magic protocol version that client is using to request a SSL session.
+const SSLMagic = 80877103
+
+// DefaultProtocolVersion is a default protocol version of PostgreSQL since version 7
+const DefaultProtocolVersion = 196608
+
+// StartupMessage represents the first message a client sends to the DB after establishing a connection.
+// This message includes the names of the user and of the database the user wants to connect to;
+// it also identifies the particular protocol version to be used as well as any additional run-time parameters.
+type StartupMessage struct {
+	ProtocolVersion int32
+	Parameters      []StartupMessageParameter // NB: we want to preserve order so we can't use map here
+}
+
+// StartupMessageParameter represents run-time parameter in a StartupMessage.
+type StartupMessageParameter struct {
+	Name  string
+	Value string
+}
+
+// Marshall serializes the message to send over network.
+func (m *StartupMessage) Marshall() ([]byte, error) {
+	var paramsBuffer WriteBuffer
+
+	// Dictionary is encoded as string key-value pair + ending /0x00
+	for _, param := range m.Parameters {
+		paramsBuffer.WriteString(param.Name)
+		paramsBuffer.WriteString(param.Value)
+	}
+
+	if paramsBuffer.Len() > 0 {
+		paramsBuffer.WriteByte(0)
+	}
+
+	// Total message length including length itself
+	messageLength := paramsBuffer.Len() + 8 // 8 = length (int32) + protocol version (int32)
+	messageBuffer := make(WriteBuffer, 0, messageLength)
+
+	// NB: StartupMessage does not have a leading type byte!
+	messageBuffer.WriteInt32(int32(messageLength))
+	messageBuffer.WriteInt32(m.ProtocolVersion)
+	messageBuffer.WriteBytes(paramsBuffer)
+
+	return messageBuffer, nil
+}
---
+++ src/pg/startup_message_test.go
@@ -0,0 +1,49 @@
+package pg
+
+import "testing"
+
+import "gotest.tools/assert"
+
+// Real SSL request packet from psql captured with Wireshark
+const GoldenSSLRequestMessagePacket = "\x00\x00\x00\x08\x04\xd2\x16\x2f"
+
+// Real startup message packet from psql captured with Wireshark
+// Parameters:
+//   user = hired
+//   database = hired_dev
+//   application_name = psql
+//   client_encoding = UTF8
+const GoldenStartupMessagePacket = "\x00\x00\x00\x52\x00\x03\x00\x00\x75\x73\x65\x72\x00\x68\x69\x72" +
+	"\x65\x64\x00\x64\x61\x74\x61\x62\x61\x73\x65\x00\x68\x69\x72\x65" +
+	"\x64\x5f\x64\x65\x76\x00\x61\x70\x70\x6c\x69\x63\x61\x74\x69\x6f" +
+	"\x6e\x5f\x6e\x61\x6d\x65\x00\x70\x73\x71\x6c\x00\x63\x6c\x69\x65" +
+	"\x6e\x74\x5f\x65\x6e\x63\x6f\x64\x69\x6e\x67\x00\x55\x54\x46\x38" +
+	"\x00\x00"
+
+func TestStartupMessageMarshall(t *testing.T) {
+	// Test SSL request
+	msg := &StartupMessage{
+		ProtocolVersion: SSLMagic,
+	}
+
+	packet, err := msg.Marshall()
+
+	assert.NilError(t, err)
+	assert.DeepEqual(t, packet, []byte(GoldenSSLRequestMessagePacket))
+
+	// Test regular start up
+	msg = &StartupMessage{
+		ProtocolVersion: DefaultProtocolVersion,
+		Parameters: []StartupMessageParameter{
+			{Name: "user", Value: "hired"},
+			{Name: "database", Value: "hired_dev"},
+			{Name: "application_name", Value: "psql"},
+			{Name: "client_encoding", Value: "UTF8"},
+		},
+	}
+
+	packet, err = msg.Marshall()
+
+	assert.NilError(t, err)
+	assert.DeepEqual(t, packet, []byte(GoldenStartupMessagePacket))
+}
