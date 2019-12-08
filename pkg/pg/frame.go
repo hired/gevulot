eadc849fb2b52763af56c4802165084e33c2fdad
Denis Diachkov <d.diachkov@gmail.com>
2019-12-08 02:13:29 -0800

    Add pg.Frame interface

---
+++ pkg/pg/frame.go
@@ -0,0 +1,15 @@
+package pg
+
+// A Frame is a raw PostgreSQL message sent over the the network to/from client/DB.
+// See https://www.postgresql.org/docs/9.6/protocol-overview.html#PROTOCOL-MESSAGE-CONCEPTS for details.
+// All frame types must implement this interface.
+type Frame interface {
+	// MessageType returns this frame's message type
+	MessageType() byte
+
+	// MessageBody returns message body bytes.
+	MessageBody() []byte
+
+	// Bytes returns raw bytes of the frame ready to be sent over the network.
+	Bytes() []byte
+}
